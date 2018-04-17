fe97f554f9f11b59eb4c2e3f0d786001913dcf64
v-vemara <v-vemara@H1A-3F318-BS1L>
2018-04-17 17:16:23 +0530

    test

---
+++ TestWebCore/TestWebCore/ClientApp/app/components/mytestlink/mytestlink.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'mytestlink1',
+    templateUrl: './mytestlink.component.html'
+})
+export class MyTestLinkComponent {
+    public forecasts: WeatherForecast[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+            this.forecasts = result.json() as WeatherForecast[];
+        }, error => console.error(error));
+    }
+    //Test Comments
+}
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
